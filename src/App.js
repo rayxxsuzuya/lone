9e5270d285ac904fad7a4b489bd8a9ab0e6d424c
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2021-02-12 03:02:07 +0300

    1.1 setstate lsn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    count: 0
+  }
+
+  add = () => {
+    this.setState(current => ({count: current.count + 1}))
+  }
+  
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Текущее число: {this.state.count}</h1>
+        <button onClick={ this.add }>Плюс</button>
+        <button onClick={ this.minus }>Минус</button>
+      </div>
+    );
+  }
 }
 
 export default App;
